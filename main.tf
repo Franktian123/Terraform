56549a48286c9bd0f2114757ff69d5eba50d5d21
Franktian123 <82508128+Franktian123@users.noreply.github.com>
2023-04-27 15:50:41 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,4 +127,12 @@ resource "aws_launch_configuration" "as_conf" {
   name          = "web_config"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+
+metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  options {
+      instance_metadata_tags = "enabled"
+  }
 }
