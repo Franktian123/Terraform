0938165b7a9d1ce3792128decc5b3aa9d7f85254
Franktian123 <82508128+Franktian123@users.noreply.github.com>
2023-06-14 23:55:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -157,28 +157,34 @@ data "aws_iam_policy_document" "sns_topic_policy" {
 
 # 5. Ensure Amazon Simple Queue Service (SQS) is not exposed to public
 
-resource "aws_sqs_queue" "mysqs" {
-  name = "test-sqs"
+resource "aws_sqs_queue" "q" {
+  name = "examplequeue"
+}
+
+data "aws_iam_policy_document" "test" {
+  statement {
+    sid    = "First"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.q.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_sns_topic.example.arn]
+    }
+  }
 }
 
 resource "aws_sqs_queue_policy" "test" {
-  queue_url = "${aws_sqs_queue.mysqs.id}"
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "*",
-    }
-  ]
-}
-POLICY
+  queue_url = aws_sqs_queue.q.id
+  policy    = data.aws_iam_policy_document.test.json
 }
 
 # 6. Ensure there are no public file systems for AWS Elastic File System (EFS)
