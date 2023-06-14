94ecd2b7188a04c92a3e5badbcc1aee09f38b36e
Franktian123 <82508128+Franktian123@users.noreply.github.com>
2023-06-14 22:53:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,3 +89,210 @@ resource "aws_kms_key" "test-KMS" {
 }
 POLICY
 }
+
+
+# 4. Ensure public access is disabled for Amazon Simple Notification Service (SNS)
+
+resource "aws_sns_topic" "test-sns" {
+  name = "my-topic-with-policy"
+  policy = jsonencode(
+         {
+             Id        = "ID-GD-Topic-Policy"
+             Statement = [
+                 {
+                     Action    = "sns:Publish"
+                     Effect    = "Allow"
+                     Principal = {
+                         Service = "events.amazonaws.com"
+                     }
+                     Resource  = aws_sns_topic.gd_sns_topic.arn
+                     Sid       = "SID-GD-Example"
+                 },
+             ]
+             Version   = "2012-10-17"
+         }
+     )
+}
+
+resource "aws_sns_topic" "test" {
+  name = "my-topic-with-policy"
+}
+
+resource "aws_sns_topic_policy" "default" {
+  arn = "${aws_sns_topic.test.arn}"
+
+  policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        "${var.account-id}",
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      "${aws_sns_topic.test.arn}",
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
+
+# 5. Ensure Amazon Simple Queue Service (SQS) is not exposed to public
+resource "aws_sqs_queue" "sqs_queue" {
+  name                      = var.trailQueueName
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  kms_master_key_id         = aws_kms_key.primary.arn
+
+  depends_on = [
+    aws_s3_bucket.cloudtrailbucket,
+    aws_kms_key.primary
+  ]
+}
+
+resource "aws_sqs_queue_policy" "sqs_queue_policy" {
+  queue_url = aws_sqs_queue.sqs_queue.id
+  policy    = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "CloudTrailSQS",
+      "Effect": "Allow",
+      "Principal": {
+          "Service": "s3.amazonaws.com"
+      },
+      "Action": [
+          "SQS:SendMessage"
+      ],
+      "Resource": "*",
+    }
+  ]
+}
+POLICY
+}
+
+
+# 6. Ensure there are no public file systems for AWS Elastic File System (EFS)
+
+resource "aws_efs_file_system" "fs" {
+  creation_token = "my-product"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = "ExampleStatement01"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "elasticfilesystem:ClientMount",
+      "elasticfilesystem:ClientWrite",
+    ]
+
+    resources = [aws_efs_file_system.fs.arn]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["true"]
+    }
+  }
+}
+
+resource "aws_efs_file_system_policy" "policy" {
+  file_system_id = aws_efs_file_system.fs.id
+  policy         = data.aws_iam_policy_document.policy.json
+}
+
+# 7. Ensure permissions are tightly controlled for Amazon Elastic Container Registry (Amazon ECR)
+
+resource "aws_ecr_repository" "foo" {
+  name = "bar"
+}
+
+data "aws_iam_policy_document" "foopolicy" {
+  statement {
+    sid    = "new policy"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "foopolicy" {
+  repository = aws_ecr_repository.foo.name
+  policy     = data.aws_iam_policy_document.foopolicy.json
+}
+
+# 8. Ensure 'password policy' is enabled - at least 1 lower case character
+
+resource "aws_cognito_user_pool" "test-passpolicy" {
+  # current
+  admin_create_user_config {
+    unused_account_validity_days: 7
+  }
+  # proposed
+  password_policy {
+    temporary_password_validity_days: 7
+    minimum_length: 7
+    require_uppercase: true
+    require_lowercase: true
+    require_numbers: true
+    require_symbols: true
+  }
+}
