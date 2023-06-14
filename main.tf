5527f52da49367ce3ff24ee6ceea034a6297ab4f
Franktian123 <82508128+Franktian123@users.noreply.github.com>
2023-06-14 21:30:49 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. for Ensure permissions are tightly controlled for AWS ElasticSearch Domains
+# 1. Ensure permissions are tightly controlled for AWS ElasticSearch Domains
 
 variable "domain" {
   default = "tf-test"
@@ -35,6 +35,52 @@ resource "aws_elasticsearch_domain" "example" {
       "Principal": "*",
       "Effect": "Allow",
       "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${var.domain}/*",
+      "Condition": {
+        "IpAddress": {"aws:SourceIp": ["66.193.100.22/32"]}
+      }
+    }
+  ]
+}
+POLICY
+}
+
+
+#2. Ensure that IAM Users are Unauthorized to Edit Access Policies
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+#3. Ensure principal is defined for every IAM policy attached to AWS Key Management Service (KMS) key
+
+resource "aws_kms_key" "test-KMS" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "iam:*",
+      "Principal": "*",
+      "Effect": "Allow",
       "Condition": {
         "IpAddress": {"aws:SourceIp": ["66.193.100.22/32"]}
       }
